7739a66627f4348ca618206526d3b1e7f9dac8dc
Anna Chernyshova <anna.chernyshova@docker.com>
2024-11-29 10:29:23 -0500

    deleted all tests to test auto test generation

--- src/test/resources/test-data.sql
+++
@@ -1,9 +0,0 @@
-DELETE
-FROM todos;
-
-insert into todos (id, title, completed, order_number)
-values ('1', 'Buy groceries', false, 1),
-       ('2', 'Prepare presentation', true, 2),
-       ('3', 'Finish SQL script', false, 3),
-       ('4', 'Book flight tickets', false, 4),
-       ('5', 'Write blog post', true, 5)
